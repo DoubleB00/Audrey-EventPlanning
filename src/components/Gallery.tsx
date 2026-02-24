59ff8cde778d75ee3cf9c115c439ead6aa48f885
DoubleB00 <bbrian0325@gmail.com>
2026-02-24 12:58:28 -0500

    Updated Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -8,8 +8,8 @@ const galleryItems = [
     category: 'Treats'
   },
   {
-    src: '/image1.jpeg',
-    alt: 'Chocolate covered strawberries and dipped oreos',
+    src: '/att.cBfMgj5SVP1jAJcM7Xga2QkuY41GbAdedXD8db9MIuA.JPG',
+    alt: "Chocolate covered strawberries and macarons dessert display by Audrey's Event Planning & Treatmaking",
     category: 'Treats'
   },
   {
